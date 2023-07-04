74e413bdb0a6b6fa5b8ac99684c76a48d379a4d7
Shawn Wilson <wilson.ohyeah@gmail.com>
2023-07-04 15:29:42 -0600

    chore: get TW and daisyUI installed, prep the  app for development. remove default installation things

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-8">
+      <h1 className="text-4xl font-bold">DaisyDocReact</h1>
+      <button className="btn btn-neutral">Hello!</button>
+      <img src="react-logo.png" className="w-24 h-24" alt="logo" />
+      <img src="daisyui-logo.png" className="w-24 h-24" alt="logo" />
     </div>
   );
 }
---
+++ ui/src/components/ComponentNav.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ComponentNavProps {}
+
+const ComponentNav: React.FC<ComponentNavProps> = () => {
+  return <div>This is a basic component.</div>;
+};
+
+export default ComponentNav;
