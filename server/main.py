dca0c8920d7c1a6b0def679ef6809210fe34143c
Shawn Wilson <wilson.ohyeah@gmail.com>
2023-07-05 22:23:07 -0600

    feat: new route/blueprint used to get/set basic system information, like the docs_url that will be used to scrape data

--- server/main.py
+++ server/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, send_from_directory
+from flask_cors import CORS
+from routes.system import system_routes
 import os
 
 app = Flask(__name__, static_folder='../ui/build')
+app.register_blueprint(system_routes)
+CORS(app)
+docs_url = "https://daisyui.com/"
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -10,6 +15,7 @@ def serve(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
